33f07e5c74091b14dc30b788c4645fafecceb89c
Cassandra Torske <cassandra.torske@gmail.com>
2017-10-15 14:47:57 -0600

    specify acceptable values for db in type

--- migrations/20171015141245_notification.js
+++ migrations/20171015141245_notification.js
@@ -4,7 +4,7 @@ exports.up = function(knex, Promise) {
     .createTable('notification', function(table){
       table.increments('id').primary()
       table.string('message', 140)
-      table.string('type').notNullable()
+      table.enu('type', ['change_machine_empty', 'broken_machine', 'other']).notNullable()
       table.dateTime('created').defaultTo(knex.fn.now())
     })
 };
