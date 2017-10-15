55f44bbcd3ac1ef01f8e696a2f9470a1a1dcece7
Cassandra Torske <cassandra.torske@gmail.com>
2017-10-15 17:13:05 -0600

    possibly validating

--- routes/index.js
+++ routes/index.js
@@ -8,6 +8,19 @@ require('dotenv').config()
 
 const client = new twilio(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_TOKEN);
 
+function validNotification(notification) {
+  if(!notification.message){
+    return true
+  }
+  if(notification.message){
+    const hasMessage = typeof notification.message == 'string'
+    const hasLength = notification.message.length <= 140
+    return hasLength && hasMessage
+  }
+}
+
+
+
 router.get('/', (req, res) => {
   queries.getAll().then(notifications => {
     res.json(notifications)
@@ -15,18 +28,25 @@ router.get('/', (req, res) => {
 })
 
 router.post('/', (req, res, next) => {
-  queries.createNotification(req.body).then(notifications => {
-    client.messages.create({
-    body: `Type: ${notifications[0].type} Message: ${notifications[0].message}`,
-    to: '+19709855659',  // Add Official Number to .env
-    from: process.env.TWILIO_NUMBER // Leave this one alone.
+  console.log(req.body.message);
+  if(validNotification(req.body.message)) {
+    queries.createNotification(req.body).then(notifications => {
+    if(notifications[0].message) {
+      if(validNotification(notifications[0])){
+        client.messages.create({
+        body: `Type: ${notifications[0].type} Message: ${notifications[0].message}`,
+        to: '+19709855659',  // Add Official Number to .env
+        from: process.env.TWILIO_NUMBER // Leave this one alone.
+        })
+      .then((message) => console.log(message.sid, message));
+          res.json('Success!')
+      }
+    }
+    }).catch(error => {
+      console.log(error);
+      res.status(500).json({message: "Failure..."})
     })
-  .then((message) => console.log(message.sid, message));
-      res.json('Success!')
-  }).catch(error => {
-    console.log(error);
-    res.status(500).json({message: "Failure..."})
-  })
+  }
 })
 
 module.exports = router
